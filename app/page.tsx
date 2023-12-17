6cf156e248b2690d780eb2bd9d93546eec8f3d52
Luanps20524 <luanchps20524@fpt.edu.vn>
2023-12-17 18:20:28 +0700

    asd

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,6 @@ export default function Home({
                     dispatch(setLoggedIn(true))
                 } else {
                     router.replace('/login')
-
                 }
             } catch (error) {
                 console.log(error)
