4727624a7bbfc06a082042b96810af156ad1babd
Luanps20524 <luanchps20524@fpt.edu.vn>
2023-12-17 21:28:46 +0700

    adsa

--- services/user.ts
+++ services/user.ts
@@ -1,7 +1,6 @@
 import User from "@/interfaces/user";
 import api from "./api";
 import { HistoryOrder, HistoryOrderAdmin } from "@/interfaces/history_order";
-
 export async function loginUser(email: string, password: string) {
   const res = await api.post("/auth/login", {
     email: email,
@@ -9,16 +8,16 @@ export async function loginUser(email: string, password: string) {
   });
   return res.data;
 }
-
 export async function getUser(access_token: string) {
   const res = await api.get("/auth/me", {
     headers: {
       Authorization: "Bearer " + access_token,
     },
   });
-  const data: User = res.data.data;
+  const data: User = res.data.data
   return data;
 }
+
 
 export async function getUserManager() {
   const res = await api.get(`/user`);
